6dbbedf9aad010d6754ee6b8adc6e88afab36b32
joaovictorjv-svg <joaovictorziks@gmail.com>
2026-08-10 15:26:31 -0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -101,7 +101,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@500;600;700&family=Barlow:wght@400;500;600;700&display=swap",
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/prev-icon.png", type: "image/x-icon" },
     ],
   }),
   shellComponent: RootShell,
